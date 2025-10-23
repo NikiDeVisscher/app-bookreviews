25de59aec79d7ed55eaab14f8b46ab823630016f
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-10-23 10:06:03 +0200

    Testing delta

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,10 +1,13 @@
 export default [
   {
     match: {
-      subject: {},
+      predicate: {
+        type: "uri",
+        value: "http://schema.org/reviewRating",
+      },
     },
     callback: {
-      url: "http://resource/.mu/delta",
+      url: "http://rating-service/delta",
       method: "POST",
     },
     options: {
@@ -13,5 +16,41 @@ export default [
       foldEffectiveChanges: true,
       ignoreFromSelf: true,
     },
-  }
+  },
+  {
+    match: {
+      predicate: {
+        type: "uri",
+        value: "http://schema.org/reviewContent",
+      },
+    },
+    callback: {
+      url: "http://rating-service/delta",
+      method: "POST",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 1000,
+      foldEffectiveChanges: true,
+      ignoreFromSelf: true,
+    },
+  },
+  {
+    match: {
+      predicate: {
+        type: "uri",
+        value: "http://schema.org/about",
+      },
+    },
+    callback: {
+      url: "http://rating-service/delta",
+      method: "POST",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 1000,
+      foldEffectiveChanges: true,
+      ignoreFromSelf: true,
+    },
+  },
 ];
--- config/rating-service/app.js
+++ config/rating-service/app.js
@@ -1,5 +1,53 @@
-import { app } from 'mu';
+import bodyParser from "body-parser";
+import { app } from "mu";
 
-app.get('/hello', function( req, res ) {
-  res.send('Hello mu-javascript-template');
-} );
+app.use(bodyParser.json({ type: '*/*' }));
+
+app.post("/delta", async (req, res) => {
+  console.log("Received delta", JSON.stringify(req.body, null, 2));
+
+  console.log("Headers:", req.headers);
+  console.log("Raw body:", req.rawBody);
+  console.log("Parsed body:", JSON.stringify(req.body, null, 2));
+
+  const inserts = req.body.delta?.[0]?.inserts || [];
+  const reviewTriples = inserts.filter(t => t.predicate.value === "http://schema.org/about");
+
+  for (const triple of reviewTriples) {
+    const reviewUri = triple.subject.value;
+    const bookUri = triple.object.value;
+
+    console.log(`Review ${reviewUri} is about book ${bookUri}`);
+
+    const reviewsResponse = await fetch(
+      `http://resource/reviews?filter[book]=${encodeURIComponent(bookUri)}`
+    );
+    const reviewsJson = await reviewsResponse.json();
+
+    const ratings = reviewsJson.data.map(r => r.attributes["review-rating"]);
+    const avg = ratings.reduce((a, b) => a + b, 0) / ratings.length;
+
+    const bookId = bookUri.split("/").pop();
+    await fetch(`http://resource/books/${bookId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/vnd.api+json",
+      },
+      body: JSON.stringify({
+        data: {
+          type: "books",
+          id: bookId,
+          attributes: {
+            "average-rating": avg,
+          },
+        },
+      }),
+    });
+
+    console.log(`Updated book ${bookId} average rating to ${avg}`);
+  }
+
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("rating-service listening on port 3000"));
